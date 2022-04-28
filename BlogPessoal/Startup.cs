39b4aabc48ae7af39922b67af0af7fdc4cfe5de1
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-04-28 15:02:59 -0300

    [Task: Criação de tabela com ORG EF] {28/04/22 15;00}

--- BlogPessoal/Startup.cs
+++ BlogPessoal/Startup.cs
@@ -1,6 +1,9 @@
+using BlogPessoal.src.data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BlogPessoal
 {
@@ -24,19 +28,23 @@ namespace BlogPessoal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuração banco de dados
             IConfigurationRoot config = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddJsonFile("appsettings.json")
                 .Build();
+            services.AddDbContext<BlogPessoalContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
 
+            //Configuração controladores
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContext context)
         {
             if (env.IsDevelopment())
             {
+                context.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
 
---
+++ BlogPessoal/src/data/BlogPessoalContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogPessoal.src.modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPessoal.src.data
+{
+    public class BlogPessoalContext : DbContext
+    {
+        public DbSet<UsuarioModelo> Usuario { get; set; }
+        public DbSet<TemaModelo> Temas { get; set; }
+        public DbSet<PostagemModelo> Postagens { get; set; }
+
+        public BlogPessoalContext(DbContextOptions<BlogPessoalContext> opt) : base(opt)
+        { 
+        
+        }   
+    }
+}
