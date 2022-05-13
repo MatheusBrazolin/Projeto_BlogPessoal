929d4afea4e67d37163de656548b4044db822800
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-05-13 14:17:27 -0300

    [Task: RESOLVE conflicts controller]

--- BlogPessoal/src/controladores/TemaControlador.cs
+++ BlogPessoal/src/controladores/TemaControlador.cs
@@ -1,11 +1,8 @@
 ﻿using BlogPessoal.src.dtos;
 using BlogPessoal.src.repositorios;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using System.Threading.Tasks;
 
-=======
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
 namespace BlogPessoal.src.controladores
 {
     [ApiController]
@@ -23,36 +20,23 @@ namespace BlogPessoal.src.controladores
         }
         #endregion
         #region Métodos
-        [HttpGet]
-<<<<<<< HEAD
+
         [HttpGet]
         public async Task<ActionResult> PegarTodosTemasAsync()
         {
             var lista = await _repositorio.PegarTodosTemasAsync();
-=======
-        public IActionResult PegarTodosTemas()
-        {
-            var lista = _repositorio.PegarTodosTemas();
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
             if (lista.Count < 1) return NoContent();
             return Ok(lista);
         }
 
         [HttpGet("id/{idTema}")]
-<<<<<<< HEAD
         public async Task<ActionResult> PegarTemaPeloIdAsync([FromRoute] int idTema)
         {
             var tema = await _repositorio.PegarTemaPeloIdAsync(idTema);
-=======
-        public IActionResult PegarTemaPeloId([FromRoute] int idTema)
-        {
-            var tema = _repositorio.PegarTemaPeloId(idTema);
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
             if (tema == null) return NotFound();
             return Ok(tema);
         }
 
-<<<<<<< HEAD
         [HttpGet("pesquisa")]
         public async Task<ActionResult> PegarTemaPelaDescricaoAsync([FromQuery] string descricaoTema)
         {
@@ -81,23 +65,6 @@ namespace BlogPessoal.src.controladores
             await _repositorio.DeletarTemaAsync(idTema);
             return NoContent();
         }
-=======
-        [HttpGet]
-        public IActionResult PegarTemaPelaDescricao([FromQuery] string descricaoTema)
-        {
-            var temas = _repositorio.PegarTemaPelaDescricao(descricaoTema);
-            if (temas.Count < 1) return NoContent();
-            return Ok(temas);
-        }
-
-        [HttpPut]
-        public IActionResult AtualizarTema([FromBody] AtualizarTemaDTO tema)
-        {
-            if (!ModelState.IsValid) return BadRequest();
-            _repositorio.AtualizarTema(tema);
-            return Ok(tema);
-        }
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
         #endregion
     }
 }
--- BlogPessoal/src/controladores/UsuarioControlador.cs
+++ BlogPessoal/src/controladores/UsuarioControlador.cs
@@ -4,10 +4,7 @@ using BlogPessoal.src.servicos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-<<<<<<< HEAD
 using System.Threading.Tasks;
-=======
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
 
 namespace BlogPessoal.src.controladores
 {
@@ -31,49 +28,28 @@ namespace BlogPessoal.src.controladores
         #region Métodos
 
         [HttpGet("id/{idUsuario}")]
-<<<<<<< HEAD
         public async Task<ActionResult> PegarUsuarioPeloIdAsync([FromRoute] int idUsuario)
         {
             var usuario = await _repositorio.PegarUsuarioPeloIdAsync(idUsuario);
-=======
-        public IActionResult PegarUsuarioPeloId([FromRoute] int idUsuario)
-        {
-            var usuario = _repositorio.PegarUsuarioPeloId(idUsuario);
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
             if (usuario == null) return NotFound();
             return Ok(usuario);
         }
 
         [HttpGet]
-<<<<<<< HEAD
         public async Task<ActionResult> PegarUsuariosPeloNomeAsync([FromQuery] string nomeUsuario)
         {
             var usuarios = await _repositorio.PegarUsuarioPeloNomeAsync(nomeUsuario);
-=======
-        public IActionResult PegarUsuarioPeloNome([FromQuery] string nomeUsuario)
-        {
-            var usuarios = _repositorio.PegarUsuarioPeloNome(nomeUsuario);
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
             if (usuarios.Count < 1) return NoContent();
             return Ok(usuarios);
         }
 
         [HttpGet("email/{emailUsuario}")]
-<<<<<<< HEAD
         public async Task<ActionResult> PegarUsuarioPeloEmailAsync([FromRoute] string emailUsuario)
         {
             var usuario = await _repositorio.PegarUsuarioPeloEmailAsync(emailUsuario);
-=======
-        public IActionResult PegarUsuarioPeloEmail([FromRoute] string emailUsuario)
-        {
-            var usuario = _repositorio.PegarUsuarioPeloEmail(emailUsuario);
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
             if (usuario == null) return NotFound();
             return Ok(usuario);
         }
-
-<<<<<<< HEAD
-
 
         [HttpPost]
         public async Task<ActionResult> NovoUsuarioAsync([FromBody] NovoUsuarioDTO usuario)
@@ -82,8 +58,6 @@ namespace BlogPessoal.src.controladores
             await _repositorio.NovoUsuarioAsync(usuario);
             return Created($"api/Usuarios/{usuario.Email}", usuario);
         }
-
-       
 
         [HttpPut]
         public async Task<ActionResult> AtualizarUsuarioAsync([FromBody]
@@ -99,39 +73,6 @@ namespace BlogPessoal.src.controladores
         {
         await _repositorio.DeletarUsuarioAsync(idUsuario);
         return NoContent();
-=======
-        [HttpPost]
-        [AllowAnonymous]
-        public IActionResult NovoUsuario([FromBody] NovoUsuarioDTO usuario)
-        {
-            if (!ModelState.IsValid) return BadRequest();
-            try
-            {
-                _servicos.CriarUsuarioSemDuplicar(usuario);
-                return Created($"api/Usuarios/email/{usuario.Email}", usuario);
-            }
-            catch (Exception ex)
-            {
-                return Unauthorized(ex.Message);
-            }
-        }
-
-        [HttpPut]
-        [Authorize(Roles = "NORMAL,ADMINISTRADOR")]
-        public IActionResult AtualizarUsuario([FromBody] AtualizarUsuarioDTO usuario)
-        {
-            if (!ModelState.IsValid) return BadRequest();
-            usuario.Senha = _servicos.CodificarSenha(usuario.Senha);
-            _repositorio.AtualizarUsuario(usuario);
-            return Ok(usuario);
-        }
-
-        [HttpDelete("deletar/{idUsuario}")]
-        public IActionResult DeletarUsuario([FromRoute] int idUsuario)
-        {
-            _repositorio.DeletarUsuario(idUsuario);
-            return NoContent();
->>>>>>> e2d7a4e8d829268eb6864957c01d0e1f3e0aa580
         }
         #endregion
     }
