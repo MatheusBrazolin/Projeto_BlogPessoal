7f46eab3a52b3b98779b40910efcda52490d9157
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-05-18 15:06:51 -0300

    [Task: Add Heroku]

--- BlogPessoal/Startup.cs
+++ BlogPessoal/Startup.cs
@@ -38,28 +38,38 @@ namespace BlogPessoal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configura��p Banco de Dados
-            services.AddDbContext<BlogPessoalContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
-
-            // Configura��o Repositorios
+            // Configuraçãp Banco de Dados
+            if (Configuration["Enviroment:Start"] == "PROD")
+            {
+                services.AddEntityFrameworkNpgsql()
+                .AddDbContext<BlogPessoalContext>(
+                opt =>
+                opt.UseNpgsql(Configuration["ConnectionStringsProd:DefaultConnection"]));
+            }
+            else
+            {
+                services.AddDbContext<BlogPessoalContext>(
+                opt =>
+                opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            }
+            // Configuração Repositorios
             services.AddScoped<IUsuario, UsuarioRepositorio>();
             services.AddScoped<ITema, TemaRepositorio>();
             services.AddScoped<IPostagem, PostagemRepositorio>();
-
-            // Controladores
+            // Configuração de Controladores
             services.AddCors();
             services.AddControllers();
-
-            // Configura��o de Servi�os
+            // Configuração de Serviços
             services.AddScoped<IAutenticacao, AutenticacaoServicos>();
-
-            // Configura��o do Token Autentica��o JWTBearer
+            // Configuração do Token Autenticação JWTBearer
             var chave = Encoding.ASCII.GetBytes(Configuration["Settings:Secret"]);
-            services.AddAuthentication(a =>
+            services.AddAuthentication(
+            a =>
             {
                 a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(b =>
+            }).AddJwtBearer(
+            b =>
             {
                 b.RequireHttpsMetadata = false;
                 b.SaveToken = true;
@@ -70,43 +80,48 @@ namespace BlogPessoal
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
-            });
-
-            // Configura��o Swagger
+            }
+            );
+            // Configuração Swagger
             services.AddSwaggerGen(s =>
             {
-                s.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog Pessoal", Version = "v1" });
-
-                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Blog Pessoal",
+                    Version
+                = "v1"
+                });
+                s.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme()
                 {
                     Name = "Authorization",
                     Type = SecuritySchemeType.ApiKey,
                     Scheme = "Bearer",
                     BearerFormat = "JWT",
                     In = ParameterLocation.Header,
-                    Description = "JWT authorization header utiliza: Bearer + JWT Token",
+                    Description = "JWT authorization header utiliza: Bearer +JWT Token",
                 });
-
-                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                s.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
                 {
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new List<string>()
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                    {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                    }
+                    },
+                    new List<string>()
                     }
                 });
-
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 s.IncludeXmlComments(xmlPath);
-            });
-
+            }
+);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -120,7 +135,14 @@ namespace BlogPessoal
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogPessoal v1"));
             }
-
+            // Ambiente de produção
+            contexto.Database.EnsureCreated();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogPessoal v1");
+                c.RoutePrefix = string.Empty;
+            });
             // Ambiente de produ��o
             // Rotas
             app.UseRouting();
@@ -134,7 +156,7 @@ namespace BlogPessoal
             // Autentica��o e Autoriza��o
             app.UseAuthentication();
             app.UseAuthorization();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
